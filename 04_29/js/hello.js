4269af2475db63967664e6f950e6b18b58f8e2de
Amanda Xavier <amanda.x.nascimento@aluno.senai.br>
2024-04-29 17:21:20 -0300

    Trabalhando com JavaScript

---
+++ 04_29/js/hello.js
@@ -0,0 +1,25 @@
+alert("segundou")
+//Meu primeiro comentario
+/*Meu segundoo comentario*/
+
+//Criando uma variavel
+let texto = "Aula JavaScript"
+console.log(texto)
+let decimal = 20.8
+console.log(decimal)
+let inteiro = 20
+console.log(inteiro)
+let booleano = true
+console.log(booleano)
+//Para criar uma variavel em JavaScript nao e necessario definir um tipo
+//podendo fazer a definicao direta e atribuicao em seguida
+document.write(texto)/*escrevendo dentro da pagina html*/
+
+let num1 = "10"
+let num2 = 20
+let soma = num1 + num2 // Concatenando String "10" + 20 resultado 1020
+console.log(soma)
+
+let dia = "Segunda"
+let saudacao = "Boa Tarde"
+console.log(dia+saudacao)
