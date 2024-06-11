28452eba8bc27d4b95ccbc32aef7135bbd8a514e
Amanda Xavier <amanda.x.nascimento@aluno.senai.br>
2024-06-11 16:13:17 -0300

    Criando um CRUD com ARRAY

---
+++ loja_parafusos/server/src/App.js
@@ -0,0 +1,17 @@
+import express from "express"; //importado o express
+import ParafusoController from "./Controllers/ParafusoController.js";
+
+const server = express(); //iniciando o express
+
+server.use(express.json()); //Configurando o json
+
+server.get('/',(req,res)=>{
+    res.status(200).json("Servidor Funcionando");
+});
+
+server.get('/parafusos',ParafusoController.read);
+server.post('/parafusos',ParafusoController.create);
+server.put('/parafusos/:index', ParafusoController.update);
+server.delete('/parafusos/:index',ParafusoController.delete);
+
+server.listen(5000);
